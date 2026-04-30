8e01eed0d98f109837c774e8fd55338a953903de
Abhinaya Bandaru <abhinayabandaru8@gmail.com>
2026-04-30 14:38:12 +0530

    Remove unnecessary development and test files

--- frontend/quiz_summary_test.js
+++
@@ -1,8 +0,0 @@
-// Test file to verify quiz summary functionality
-console.log('Quiz Summary Feature Test');
-console.log('Expected: After completing quiz, should show:');
-console.log('- Number of correct answers');
-console.log('- Number of incorrect answers');
-console.log('- Total questions');
-console.log('- Percentage score with progress bar');
-console.log('- Back to Menu and Try Again buttons');
